57b77652e00867f59568020a8cccc1306cd31185
Daniel <danicademic@outlook.com>
2024-03-13 16:35:09 +0000

    js hecho

---
+++ starter-code/form.js
@@ -0,0 +1,48 @@
+const formulario = document.querySelector(".formu");
+// console.log(formulario)
+
+const datos = {
+  nombre: "",
+  email: "",
+  mensaje: "",
+};
+
+const nombre = document.querySelector("#nombre");
+const email = document.querySelector("#email");
+const mensaje = document.querySelector("#mensaje");
+
+console.log(nombre)
+console.log(email)
+console.log(mensaje)
+
+nombre.addEventListener("input", leerTexto);
+email.addEventListener("input", leerTexto);
+mensaje.addEventListener("input", leerTexto);
+
+function leerTexto (e){
+    datos[e.target.id] = e.target.value;
+    console.log(e)
+}
+
+// formulario.addEventListener('submit', comprobarDatos());
+formulario.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  const { nombre, email, mensaje } = datos;
+
+  if (nombre == "" || email == "" || mensaje == "") {
+    mensajeEnviar("Todos los campos son obligatorios", "error");
+    return;
+  }
+  mensajeEnviar("Mensaje enviado correctamente", 'ok')
+});
+
+const mensajeEnviar = (mensaje, clase) => {
+  const mostrar = document.createElement("p");
+  mostrar.textContent = mensaje;
+  mostrar.classList.add(clase);
+  formulario.appendChild(mostrar);
+  setTimeout(() => {
+    mostrar.remove();
+  }, 2000);
+};
